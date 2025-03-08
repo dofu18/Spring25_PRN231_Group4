04cedd3ca4dc52ac54f8dd94684b491669f1890e
Trung <116988878+K16Trung@users.noreply.github.com>
2025-03-08 14:46:54 +0700

    Add category, coursecategory, lesson

---
+++ InfrastructureLayer/Repository/CourseCategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using DomainLayer.Entities;
+using InfrastructureLayer.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfrastructureLayer.Repository
+{
+    public class CourseCategoryRepository : GenericRepository<CourseCategory>, ICourseCategoryRepository
+    {
+        private readonly TutoringKidDbContext _tutoringKidDbContext;
+        public CourseCategoryRepository(TutoringKidDbContext tutoringKidDbContext) : base(tutoringKidDbContext)
+        {
+            _tutoringKidDbContext = tutoringKidDbContext;
+        }
+    }
+}
---
+++ InfrastructureLayer/Repository/IRepository/ICourseCategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using DomainLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfrastructureLayer.Repository.IRepository
+{
+    public interface ICourseCategoryRepository : IGenericRepository<CourseCategory>
+    {
+    }
+}
